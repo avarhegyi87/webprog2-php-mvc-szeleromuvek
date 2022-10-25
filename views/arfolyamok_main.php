e5e1ec83c0d05651d1ca6fa970a139cae8073a45
csabaialbert <68478226+csabaialbert@users.noreply.github.com>
2022-10-25 18:34:45 +0200

    mnb_napi

---
+++ views/arfolyamok_main.php
@@ -0,0 +1,11 @@
+
+<ul>
+            <li >
+              <a href="./views/devizapar_main.php" >Devizapár árfolyam lekérdezés (egy megadott napra)</a>
+            </li>
+            <li >
+              <a href="#" >Devizapár árfolyam lekérdezés (egy megadott intervallumra)</a>
+            </li>
+          
+</ul>
+
